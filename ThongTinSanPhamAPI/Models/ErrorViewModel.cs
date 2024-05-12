e4cab39a6d20bb22bd7ba95997b06c2d7ae71227
Hoàng Nguyên <110187720+hoangnguyenhtng@users.noreply.github.com>
2024-05-12 18:44:38 +0700

    part 1

---
+++ ThongTinSanPhamAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThongTinSanPhamAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
